909c97051c2ceb99a74d598e66e888c27fafa414
areeba-rehman <106922554+areeba-rehman@users.noreply.github.com>
2024-03-15 21:05:53 +0500

    implemented style

---
+++ color-flipper/script.js
@@ -0,0 +1,12 @@
+
+const colors = ["red", "pink", "green", "yellow"];
+const colorBox = document.getElementById("color-box");
+
+let currentIndex = 0;
+
+colorBox.addEventListener("click", () => {
+    currentIndex = (currentIndex + 1) % colors.length;
+    colorBox.style.backgroundColor = colors[currentIndex];
+    colorBox.textContent = colors[currentIndex].toUpperCase();
+});
+
