72506abfe7450fc89c289328ef1ed85c5184bf0c
areeba-rehman <106922554+areeba-rehman@users.noreply.github.com>
2024-03-16 21:19:03 +0500

    changes done

--- modal project/script.js
+++ modal project/script.js
@@ -2,17 +2,12 @@ const modal = document.getElementById("modal");
 const closeBtn = document.getElementsByClassName("close")[0];
 
 
-function openModal() {
+
+document.getElementById("open-modal-btn").addEventListener("click", () => {
     modal.style.display = "block";
-}
+});
 
 
-function closeModal() {
+closeBtn.addEventListener("click", () => {
     modal.style.display = "none";
-}
-
-
-document.getElementById("open-modal-btn").addEventListener("click", openModal);
-
-
-closeBtn.addEventListener("click", closeModal);
+});
